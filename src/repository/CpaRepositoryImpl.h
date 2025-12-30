5c900bcea4fb093805a30d517e9478ab6edd007d
Nayan Kurude <nkurude@atlassian.com>
2025-12-30 15:03:37 +0530

    kjhdsjakhjdksa

--- src/repository/CpaRepositoryImpl.h
+++ src/repository/CpaRepositoryImpl.h
@@ -2,10 +2,45 @@
 #define _CPA_REPOSITORY_IMPL_H_
 
 #include "../CpaRepository.h"
+#include <iostream>
 
 template<typename Entity, typename ID>
 class CpaRepositoryImpl : public CpaRepository<Entity, ID> {
     Public Virtual ~CpaRepositoryImpl() = default;
+
+    // Dummy implementations that print method names
+    Public Virtual Entity Save(Entity& entity) override {
+        std::cout << "CpaRepositoryImpl::Save" << std::endl;
+        return entity;
+    }
+
+    Public Virtual optional<Entity> FindById(ID& id) override {
+        std::cout << "CpaRepositoryImpl::FindById" << std::endl;
+        return std::nullopt;
+    }
+
+    Public Virtual StdVector<Entity> FindAll() override {
+        std::cout << "CpaRepositoryImpl::FindAll" << std::endl;
+        return StdVector<Entity>();
+    }
+
+    Public Virtual Entity Update(Entity& entity) override {
+        std::cout << "CpaRepositoryImpl::Update" << std::endl;
+        return entity;
+    }
+
+    Public Virtual Void DeleteById(ID& id) override {
+        std::cout << "CpaRepositoryImpl::DeleteById" << std::endl;
+    }
+
+    Public Virtual Void Delete(Entity& entity) override {
+        std::cout << "CpaRepositoryImpl::Delete" << std::endl;
+    }
+
+    Public Virtual Bool ExistsById(ID& id) override {
+        std::cout << "CpaRepositoryImpl::ExistsById" << std::endl;
+        return false;
+    }
 };
 
 #endif // _CPA_REPOSITORY_IMPL_H_   

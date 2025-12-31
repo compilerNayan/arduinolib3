2380b012fd74a2b133f6718397bc81ef5f93388e
Nayan Kurude <nkurude@atlassian.com>
2025-12-31 11:37:40 +0530

    ldklajkl

--- src/repository/CpaRepositoryImpl.h
+++ src/repository/CpaRepositoryImpl.h
@@ -2,8 +2,10 @@
 #define _CPA_REPOSITORY_IMPL_H_
 
 #include "../CpaRepository.h"
-#include <IFileManager.h>
-#include <iostream>
+#include "../IFileManager.h"
+#include <optional>
+
+#define DATABASE_PATH "/Users/nkurude/Downloads/db"
 
 template<typename Entity, typename ID>
 class CpaRepositoryImpl : public CpaRepository<Entity, ID> {
@@ -12,38 +14,119 @@ class CpaRepositoryImpl : public CpaRepository<Entity, ID> {
     AUTOWIRED
     Protected IFileManagerPtr fileManager;
 
-    // Dummy implementations that print method names
+    // Helper method to construct file path
+    Protected StdString GetFilePath(const StdString& entityName, ID id) {
+        return StdString(DATABASE_PATH) + "/" + entityName + "_" + StdString(std::to_string(id).c_str()) + ".txt";
+    }
+
+    // Create: Save a new entity
     Public Virtual Entity Save(Entity& entity) override {
-        std::cout << "CpaRepositoryImpl::Save" << std::endl;
+        // Get entity name (static method)
+        StdString entityName = Entity::GetEntityName();
+        
+        // Get generated ID (non-static method)
+        ID generatedId = entity.GetGeneratedID();
+        
+        // Construct file path: DATABASE_PATH/EntityName_GeneratedID.txt
+        StdString filePath = GetFilePath(entityName, generatedId);
+        
+        // Serialize entity (non-static method)
+        StdString contents = entity.Serialize();
+        
+        // Save to file using file manager
+        CStdString filePathRef = filePath;
+        CStdString contentsRef = contents;
+        fileManager->Create(filePathRef, contentsRef);
+        
         return entity;
     }
 
+    // Read: Find entity by ID
     Public Virtual optional<Entity> FindById(ID& id) override {
-        std::cout << "CpaRepositoryImpl::FindById" << std::endl;
-        return std::nullopt;
+        // Get entity name (static method)
+        StdString entityName = Entity::GetEntityName();
+        
+        // Construct file path
+        StdString filePath = GetFilePath(entityName, id);
+        
+        // Read file contents
+        CStdString filePathRef = filePath;
+        StdString contents = fileManager->Read(filePathRef);
+        
+        // Check if file was read successfully (non-empty content)
+        if (contents.empty()) {
+            return std::nullopt;
+        }
+        
+        // Deserialize entity
+        Entity entity;
+        entity.Deserialize(contents);
+        
+        return entity;
     }
 
+    // Read: Find all entities
     Public Virtual vector<Entity> FindAll() override {
-        std::cout << "CpaRepositoryImpl::FindAll" << std::endl;
         return vector<Entity>();
     }
 
+    // Update: Update an existing entity
     Public Virtual Entity Update(Entity& entity) override {
-        std::cout << "CpaRepositoryImpl::Update" << std::endl;
+        // Get entity name (static method)
+        StdString entityName = Entity::GetEntityName();
+        
+        // Get ID from entity
+        ID id = entity.GetGeneratedID();
+        
+        // Construct file path
+        StdString filePath = GetFilePath(entityName, id);
+        
+        // Serialize entity
+        StdString contents = entity.Serialize();
+        
+        // Update file using file manager
+        CStdString filePathRef = filePath;
+        CStdString contentsRef = contents;
+        fileManager->Update(filePathRef, contentsRef);
+        
         return entity;
     }
 
+    // Delete: Delete entity by ID
     Public Virtual Void DeleteById(ID& id) override {
-        std::cout << "CpaRepositoryImpl::DeleteById" << std::endl;
+        // Get entity name (static method)
+        StdString entityName = Entity::GetEntityName();
+        
+        // Construct file path
+        StdString filePath = GetFilePath(entityName, id);
+        
+        // Delete file using file manager
+        CStdString filePathRef = filePath;
+        fileManager->Delete(filePathRef);
     }
 
+    // Delete: Delete an entity
     Public Virtual Void Delete(Entity& entity) override {
-        std::cout << "CpaRepositoryImpl::Delete" << std::endl;
+        // Get ID from entity
+        ID id = entity.GetGeneratedID();
+        
+        // Use DeleteById to delete
+        DeleteById(id);
     }
 
+    // Check if entity exists by ID
     Public Virtual Bool ExistsById(ID& id) override {
-        std::cout << "CpaRepositoryImpl::ExistsById" << std::endl;
-        return false;
+        // Get entity name (static method)
+        StdString entityName = Entity::GetEntityName();
+        
+        // Construct file path
+        StdString filePath = GetFilePath(entityName, id);
+        
+        // Try to read the file - if it returns non-empty, it exists
+        CStdString filePathRef = filePath;
+        StdString contents = fileManager->Read(filePathRef);
+        
+        return !contents.empty();
     }
 };
 
